e3a3438efd7537f84f0642e607d74fa5d5e9f94c
Densil Cabrera <densil.cabrera@gmail.com>
2016-09-29 17:32:38 +1000

    clearhistory basic processor

---
+++ Processors/Basic/clearhistory.m
@@ -0,0 +1,9 @@
+function OUT = clearhistory(IN)
+% This function replaces the entire history field with a row indicating
+% 'Cleared history'
+OUT = IN;
+OUT.history = cell(1,4);
+OUT.history{1,1} = datestr(now);
+OUT.history{1,2} = 'Cleared history';
+end
+    
