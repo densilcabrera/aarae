45e22b394b786adb3be55c1916f1ff1311d735d6
Densil Cabrera <densil.cabrera@gmail.com>
2017-10-06 09:30:58 +1100

    revision of room noise STI analyser so that it can do multi channel analysis (one channel at a time, so that the results in the log file are reasonably easy to understand). Also indirect STI is calculated for various NC levels (in addition to calculation for the actual background noise level)

--- Analysers/Multi Analysis/RoomNoiseSTIAnalysis.m
+++ Analysers/Multi Analysis/RoomNoiseSTIAnalysis.m
@@ -34,9 +34,9 @@ end
 
 if nargin == 1
     param = inputdlg({'Duration of impulse response [s]';...
-                       'Background noise type: Leq (0), L90 fast (1) or L90 slow (2)';...
-                       'Subtract noise level from speech level [0 | 1]'},...
-                       'Analysis parameters',1,{'3';'0';'1'});
+        'Background noise type: Leq (0), L90 fast (1) or L90 slow (2)';...
+        'Subtract noise level from speech level [0 | 1]'},...
+        'Analysis parameters',1,{'3';'0';'1'});
     param = str2num(char(param));
     if length(param) < 3, X = []; return; end
     if ~isempty(param)
@@ -74,56 +74,96 @@ for ch = 1:chans
 end
 X.properties.lags = Cindex; % include lags as a new field in output structure
 
-
-% Analyse STIPA signal to determine speech level
-STIPAresult = STIPA_direct(STIPArecording,48000,cal,[],1,1,0);
-speechlevel = STIPAresult.Level(:)';
-
-% Analyse octave band sound pressure level of silence
-%X = octave_band_level_barplot(Silencerecording,48000,0,1,16,16000,0,0,0);
-NOISEresult = RoomNoiseEvaluation_NC_RNC(Silencerecording,48000,cal,1);
-switch Noisetype
-    case 1
-        noiselevel = NOISEresult.L90fast(4:end);
-    case 2
-        noiselevel = NOISEresult.L90slow(4:end);
-    otherwise
-        noiselevel = NOISEresult.Leq(4:end);
-end
-
-% Subtract noise from speech - could cause problems if noise is not
-% constant! In benign cases this will have little effect.
-if SubtractNoise
-    subtractok = speechlevel>noiselevel;
-    speechlevel(subtractok) = pow2db(db2pow(speechlevel(subtractok)) - db2pow(noiselevel(subtractok)));
-    speechlevel(~subtractok) = -10; % -10 dB effectively no speech sound
-end
-
 % Process sweep to make an IR, which will be returned by this function
+startindex = 1444000-24000; % this can be refined
+endindex = startindex+round(IRduration*X.fs);
 X.audio = convolveaudiolocal(Sweeprecording,X.audio2);
-X.audio = X.audio(1440000:(1440000+round(IRduration*X.fs)),:,:,:,:,:);
-
-% Reverberation parameters and indirect STI
-RTresult = ReverberationTimeIR2(X,-20,1,125,8000,1,-1,1,2,-5,-15,1);
-STI_IRresult = STI_IR(X,48000,speechlevel,noiselevel,2011,1,1,2,1,0);
+X.audio = X.audio(startindex:endindex,:,:,:,:,:);
 
 % Analyse silence as if it is a sweep, to allow for effective SNR analysis
 IRsilence = convolveaudiolocal(Silencerecording,X.audio2);
-IRsilence = IRsilence(1440000:(1440000+round(IRduration*X.fs)),:,:,:,:,:);
-X.audio = cat(4,X.audio, IRsilence);
+IRsilence = IRsilence(startindex:endindex,:,:,:,:,:);
 
-% Concatenate tables
-if ~isfield(X,'tables'), X.tables = []; end
-X.tables = [X.tables STIPAresult.tables NOISEresult.tables RTresult.tables STI_IRresult.tables];
+for ch = 1:chans
+    for d4 = 1:cycles
+        for d5 = 1:outchans
+            for d6 = 1:dim6
+                % Analyse STIPA signal to determine speech level
+                STIPAresult = STIPA_direct(STIPArecording(:,ch,1,d4,d5,d6),48000,cal(ch),[],1,1,0);
+                speechlevel = permute(STIPAresult.Level,[2,3,1]);
+                
+                % Analyse octave band sound pressure level of silence
+                %X = octave_band_level_barplot(Silencerecording,48000,0,1,16,16000,0,0,0);
+                NOISEresult = RoomNoiseEvaluation_NC_RNC(Silencerecording(:,ch,1,d4,d5,d6),48000,cal(ch),1);
+                switch Noisetype
+                    case 1
+                        noiselevel = NOISEresult.L90fast(4:end);
+                    case 2
+                        noiselevel = NOISEresult.L90slow(4:end);
+                    otherwise
+                        noiselevel = NOISEresult.Leq(4:end);
+                end
+                
+                % Subtract noise from speech - could cause problems if noise is not
+                % constant! In benign cases this will have little effect.
+                if SubtractNoise
+                    subtractok = speechlevel>noiselevel;
+                    speechlevel(subtractok) = pow2db(db2pow(speechlevel(subtractok)) - db2pow(noiselevel(subtractok)));
+                    speechlevel(~subtractok) = -10; % -10 dB effectively no speech sound
+                end
+                
+                X1 = X;
+                X1.audio = X1.audio(:,ch,1,d4,d5,d6);
+                % Reverberation parameters and indirect STI
+                RTresult = ReverberationTimeIR2(X1,-20,1,125,8000,1,-1,1,2,-5,-15,1);
+                
+                STI_IRresult = STI_IR(X1,48000,speechlevel,noiselevel,2011,1,1,2,1,0);
+                
+                % STI with various NC curve background noise levels
+                noiselevel = [44 37 31 27 24 22 22]; % NC25
+                 STI_IRresultNC25 = STI_IR(X1,48000,speechlevel,noiselevel,2011,1,1,2,1,0);
+                noiselevel = [48 41 35 32 29 28 27];% NC30
+                 STI_IRresultNC30 = STI_IR(X1,48000,speechlevel,noiselevel,2011,1,1,2,1,0);
+                noiselevel = [52 45 40 36 34 33 32];% NC35
+                 STI_IRresultNC35 = STI_IR(X1,48000,speechlevel,noiselevel,2011,1,1,2,1,0);
+                noiselevel = [56 50 44 41 39 38 37];% NC40
+                 STI_IRresultNC40 = STI_IR(X1,48000,speechlevel,noiselevel,2011,1,1,2,1,0);
+                noiselevel = [60 54 49 46 44 43 42];% NC45
+                 STI_IRresultNC45 = STI_IR(X1,48000,speechlevel,noiselevel,2011,1,1,2,1,0);
+                noiselevel = [64 58 54 51 49 48 47];% NC50
+                 STI_IRresultNC50 = STI_IR(X1,48000,speechlevel,noiselevel,2011,1,1,2,1,0);
 
-X.funcallback.name = 'RoomNoiseSTIAnalysis.m';
-X.funcallback.inarg = {IRduration,Noisetype,SubtractNoise}; 
-X = rmfield(X,{'STIPAsignal','audio2'}); % remove extra audio fields to reduce data size
+                
+                % harmonic distortion - potentially could be included in
+                % future
+                % X1.audio = cat(4,IRsilence(:,ch,1,d4,d5,d6),X1.audio);
+                % X1.properties.relgain = [-inf 0];
+                % THDresult = ;
+                
+
+                % Concatenate tables
+                if ~isfield(X,'tables'), X.tables = []; end
+                X.tables = [X.tables STIPAresult.tables NOISEresult.tables ...
+                    RTresult.tables STI_IRresult.tables ...
+                    STI_IRresultNC25.tables STI_IRresultNC30.tables STI_IRresultNC35.tables ...
+                    STI_IRresultNC40.tables STI_IRresultNC45.tables STI_IRresultNC50.tables];
+            end
+        end
+    end
 end
 
+
+X.audio = cat(4,X.audio, IRsilence);
+
+X.funcallback.name = 'RoomNoiseSTIAnalysis.m';
+X.funcallback.inarg = {IRduration,Noisetype,SubtractNoise};
+X = rmfield(X,{'STIPAsignal','audio2'}); % remove extra audio fields to reduce data size
+end % eof
+
+
 function IR = convolveaudiolocal(audio1,audio2)
-    fftlen = length(audio1)+length(audio2)-1;
-    IR = ifft(fft(audio1,fftlen) .* fft(audio2,fftlen));
+fftlen = length(audio1)+length(audio2)-1;
+IR = ifft(fft(audio1,fftlen) .* fft(audio2,fftlen));
 end
 
 %**************************************************************************
